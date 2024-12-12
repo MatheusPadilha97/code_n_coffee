f6590569836505e5446250afc0541e0dfaa4365d
Matheus Padilha <matheusap11@gmail.com>
2024-12-12 12:48:16 -0300

    animações produtos e sobre

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Hamburger Nav
+
 const hamburger = document.querySelector(".hamburger")
 
 const nav = document.querySelector(".nav")
@@ -5,3 +7,36 @@ const nav = document.querySelector(".nav")
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active")
 })
+
+// Animação scroll Produtos
+
+const myObserverProducts = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elementsProdutcs = document.querySelectorAll('.hidden-produtos')
+
+elementsProdutcs.forEach((element) => myObserverProducts.observe(element))
+
+
+// Animação scroll Sobre Nós
+
+const myObserverSobre = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            entry.target.classList.add('sobre-show')
+        } else {
+            entry.target.classList.remove('sobre-show')
+        }
+    })
+})
+
+const elementsSobre = document.querySelectorAll('.sobre-nos')
+
+elementsSobre.forEach((elementSobre) => myObserverSobre.observe(elementSobre))
